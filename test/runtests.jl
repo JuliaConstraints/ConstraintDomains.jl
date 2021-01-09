0106065951137f0fc1cbe4c6a6eb48abc676a907
Azzaare <jf@baffier.fr>
2021-01-09 23:30:01 +0900

    Added domain.jl and associated tests.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,30 @@
 using ConstraintDomains
+using Dictionaries
 using Test
 
 @testset "ConstraintDomains.jl" begin
-    # Write your tests here.
+
+    d1 = domain([4,3,2,1])
+    d2 = domain([4,3,2,1]; type=:indices)
+    domains = Dictionary(1:2, [d1, d2])
+
+    # get
+    @test _get(d2, 2) == 3
+    for d in domains
+    # constructors and ∈
+        for x in [1,2,3,4]
+            @test x ∈ d
+        end
+    # length
+        @test _length(d) == 4
+    # draw and ∈
+        @test _draw(d) ∈ d
+    # add!
+        _add!(d, 5)
+        @test 5 ∈ d
+    # delete!
+        _delete!(d, 5)
+        @test 5 ∉ d
+    end
+
 end
