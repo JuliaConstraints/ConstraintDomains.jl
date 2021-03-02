b1cd0908d5834259ded9dfd7f09f567ef0b02712
azzaare <jf@baffier.fr>
2021-03-02 12:10:17 +0900

    Implement (with warning) some required function

--- src/range.jl
+++ src/range.jl
@@ -11,3 +11,8 @@ _draw(rd::RangeDomain) = rand(rd.range)
 _domain_size(rd::RangeDomain) = extrema(rd.range)
 
 domain(range::AbstractRange) = RangeDomain(range)
+
+_get_domain(d::RangeDomain) = d.range
+
+_add!(d::AbstractDomain, value) = @warn """'add!' not implement yet for """ d
+_delete!(d::AbstractDomain, value) = @warn """'delete!' not implement yet for """ d
