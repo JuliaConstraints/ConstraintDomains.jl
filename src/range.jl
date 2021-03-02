12688450aeade4a42a3bc155d9f4ca2c1d5e7605
azzaare <jf@baffier.fr>
2021-03-02 12:44:12 +0900

    Fixed abstract type in structure

--- src/range.jl
+++ src/range.jl
@@ -1,5 +1,5 @@
-struct RangeDomain{T <: Real} <: AbstractDomain
-    range::AbstractRange{T}
+struct RangeDomain{T <: Real, R <: AbstractRange{T}} <: AbstractDomain
+    range::R
 end
 
 _length(rd::RangeDomain) = length(rd.range)
@@ -10,9 +10,9 @@ _draw(rd::RangeDomain) = rand(rd.range)
 
 _domain_size(rd::RangeDomain) = extrema(rd.range)
 
-domain(range::AbstractRange) = RangeDomain(range)
+domain(range::R) where {T <: Real, R <: AbstractRange{T}} = RangeDomain{T, R}(range)
 
 _get_domain(d::RangeDomain) = d.range
 
-_add!(d::AbstractDomain, value) = @warn """'add!' not implement yet for """ d
-_delete!(d::AbstractDomain, value) = @warn """'delete!' not implement yet for """ d
+_add!(d::AbstractDomain, value) = @debug """'add!' not implement yet for """ d
+_delete!(d::AbstractDomain, value) = @debug """'delete!' not implement yet for """ d
