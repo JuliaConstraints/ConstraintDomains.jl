41b1086cd7d1c799b70449184eebefc8234bfecf
Azzaare <jf@baffier.fr>
2022-11-02 17:01:05 +0900

    Changes for parameters generation

--- src/parameters.jl
+++ src/parameters.jl
@@ -14,9 +14,9 @@ ConstraintCommons.accept(fa::FakeAutomaton, word) = word ∈ fa.words
 
 function fake_automaton(d)
     l = maximum(length, d)
-    words = Set{eltype(d)}()
+    words = Set{Vector{eltype(first(d))}}()
     for _ in 1:l
-        @info map(rand, d) words
+        # @info map(rand, d) words
         push!(words, map(rand, d))
     end
     return FakeAutomaton(words)
@@ -31,10 +31,13 @@ generate_parameters(d, ::Val{:pair_vars}) = d
 generate_parameters(d, ::Val{:val}) = rand(d)
 
 function generate_parameters(d, ::Val{:vals})
-    k = rand(1:length(d))
-    return d[1:k]
+    l = length(d)
+    k = rand(1:l)
+    return fill(d[1:k], l)
 end
 
 generate_parameters(d, param) = generate_parameters(d, Val(param))
 
 generate_parameters(d::AbstractDomain, param) = generate_parameters([d], param)
+
+Base.rand(fa::FakeAutomaton) = fa
