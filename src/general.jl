d0bc6b90e1303270c47cf13b258dd6c39235643b
azzaare <jf@baffier.fr>
2023-01-05 16:04:58 +0900

    Temp save

--- src/general.jl
+++ src/general.jl
@@ -2,12 +2,12 @@ Base.eltype(::D) where {T, D <: Union{DiscreteDomain{T}, ContinuousDomain{T}}} =
 
 function Base.convert(::Type{Intervals}, d::RangeDomain{T}) where {T <: Real}
     a, b = extrema(get_domain(d))
-    return domain((Float64(a), true), (Float64(b), true))
+    return domain(Float64(a)..Float64(b))
 end
 
 function Base.convert(::Type{RangeDomain}, d::Intervals{T}) where {T <: Real}
     i = get_domain(d)[1]
-    a = Int(value(i, :a) + (closed(i, :a) ? 0 : 1))
-    b = Int(value(i, :b) - (closed(i, :b) ? 0 : 1))
+    a = Int(i.first)
+    b = Int(i.last)
     return domain(a:b)
 end
