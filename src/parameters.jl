1a5238b7d71a96a53e79d37a205ce07f97d7435f
azzaare <jf@baffier.fr>
2023-02-06 16:52:59 +0900

    Update doc for parameters generation

--- src/parameters.jl
+++ src/parameters.jl
@@ -4,14 +4,28 @@ generate_parameters(d, ::Val{:dim}) = domain(1:sqrt(length(d)))
 
 generate_parameters(d, ::Val{:id}) = domain(1:length(d))
 
-# TODO: how to generate graph/automata ?
 
+"""
+    FakeAutomaton{T} <: ConstraintCommons.AbstractAutomaton
+
+A structure to generate pseudo automaton enough for parameter exploration.
+"""
 struct FakeAutomaton{T} <: ConstraintCommons.AbstractAutomaton
     words::Set{T}
 end
 
+"""
+    ConstraintCommons.accept(fa::FakeAutomaton, word)
+
+Implement the `accept` methods for `FakeAutomaton`.
+"""
 ConstraintCommons.accept(fa::FakeAutomaton, word) = word ∈ fa.words
 
+"""
+    fake_automaton(d)
+
+Construct a `FakeAutomaton`.
+"""
 function fake_automaton(d)
     l = maximum(length, d)
     words = Set{Vector{eltype(first(d))}}()
@@ -38,6 +52,18 @@ end
 
 generate_parameters(d, param) = generate_parameters(d, Val(param))
 
+"""
+    generate_parameters(d<:AbstractDomain, param)
+
+Generates random parameters based on the domain `d` and the kind of parameters `param`.
+"""
 generate_parameters(d::AbstractDomain, param) = generate_parameters([d], param)
 
+"""
+    Base.rand(fa::FakeAutomaton)
+
+Extends `Base.rand`. Currently simply returns `fa`.
+"""
 Base.rand(fa::FakeAutomaton) = fa
+
+# TODO - Add test items
