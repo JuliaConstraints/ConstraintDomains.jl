a0e7bf4155582bfe402cf11e52d1d062418e4a51
Azzaare <jf@baffier.fr>
2024-05-30 10:13:26 +0900

    Add Aqua

---
+++ test/Aqua.jl
@@ -0,0 +1,28 @@
+@testset "Aqua.jl" begin
+    import Aqua
+    import ConstraintDomains
+    import Dictionaries
+
+    # TODO: Fix the broken tests and remove the `broken = true` flag
+    Aqua.test_all(
+        ConstraintDomains;
+        ambiguities = (broken = false,),
+        deps_compat = false,
+        piracies = (broken = false,),
+    )
+
+    @testset "Ambiguities: ConstraintDomains" begin
+        Aqua.test_ambiguities(ConstraintDomains;)
+    end
+
+    @testset "Piracies: ConstraintDomains" begin
+        Aqua.test_piracies(ConstraintDomains;)
+    end
+
+    @testset "Dependencies compatibility (no extras)" begin
+        Aqua.test_deps_compat(
+            ConstraintDomains;
+            check_extras = false,            # ignore = [:Random]
+        )
+    end
+end
